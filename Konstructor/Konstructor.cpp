a79c985eec7827bc717b12bfe0db65ebc7bef9f7
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-14 11:38:27 +0700

    merubah dari luar kedalam dan dari dalam keluar

--- Konstructor/Konstructor.cpp
+++ Konstructor/Konstructor.cpp
@@ -15,15 +15,31 @@ public:
 		nim = iNim;
 	}
 
-	mahasiswa(string iNama) {
-		nim = 0;
-		nama = iNama;
-	}
-	mahasiswa(int iNim, string iNama) {
-		nim = iNim;
-		nama = iNama;
-	}
+	mahasiswa(string iNama);
+	mahasiswa(int iNim, string iNama); 
 	void cetak();
 };
 
 
+mahasiswa::mahasiswa(string iNama) {
+	nim = 0;
+	nama = iNama; 
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim; 
+	nama = iNama; 
+}
+
+void mahasiswa::cetak() {
+	cout << endl << "Nim =" << nim << endl;
+	cout << " Nama = " << nama << endl; 
+}
+
+int main() {
+	mahasiswa mhs1; 
+	mahasiswa mhs2(20); 
+	mahasiswa mhs3("Sayang"); 
+	mahasiswa mhs4(30, "jaddu"); 
+
+}
