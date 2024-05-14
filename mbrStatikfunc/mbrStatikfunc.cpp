7c93ae8c94570771f6d1eb57049e1ed641293a0c
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-14 13:21:03 +0700

    memasukan data

--- mbrStatikfunc/mbrStatikfunc.cpp
+++ mbrStatikfunc/mbrStatikfunc.cpp
@@ -30,3 +30,12 @@ void mahasiswa::setID() {
 	cout << endl;
 }
 
+int main() {
+	mahasiswa mhs1("Sri Dadi"); 
+	mahasiswa mhs2("Budi Jatmiko"); 
+	mahasiswa::setNim(9); // mengakses nim melalui static member function "setNim"
+	mahasiswa mhs3("Andi Janu"); 
+	mahasiswa mhs4("Joko Wahono"); 
+
+}
+
