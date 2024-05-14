7d561c879db99cd0f0bee84bf796510369a3cecc
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-14 10:45:39 +0700

    definisi

--- stuctifunction/stuctifunction.cpp
+++ stuctifunction/stuctifunction.cpp
@@ -10,4 +10,10 @@ public:
 	string nama; 
 
 	void setID(); 
+	void printall(); 
+
+	static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
+	static int getNim() { return nim; /*Definisi Function*/ }
+
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
